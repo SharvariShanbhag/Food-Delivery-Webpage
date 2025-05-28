6929362221e14f89e46fcd2fcdcb4be56d69f0ef
SharvariShanbhag <sharvarishanbhag211@gmail.com>
2025-05-29 00:33:36 +0530

    Hello to my Food Delivery webpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,11 @@ export default function Header() {
     <header className="header">
       {/* Text Section */}
       <div className="header-text">
-        <h1>Ignite Your Digital Presence</h1>
-        <p>
-          Build blazing-fast, beautiful web experiences that captivate users and
-          leave a lasting impression. Let’s make your vision a vivid reality.
-        </p>
+        <h1>Where Hunger Meets Happiness</h1>
+<p>
+  From sizzling street eats to gourmet treats - we bring the city's flavors straight to your doorstep. Bite in, bliss out, and let your cravings lead the way.
+</p>
+
         <button>Get Started</button>
       </div>
 
